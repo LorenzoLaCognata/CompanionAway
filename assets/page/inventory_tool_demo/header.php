f88746b6aedce8c7e510b95fab5a24104f930f72
Lorenzo La Cognata <vodevil@live.com>
2026-08-10 13:51:46 +0200

    Fixed arrow symbol for compatibility

--- assets/page/inventory_tool_demo/header.php
+++ assets/page/inventory_tool_demo/header.php
@@ -4,7 +4,7 @@
 
 			<div class="top-bar">
 				<div class="top-bar-left">
-					<a href="<?= $homeSlug ?>" class="top-bar-back" aria-label="<?= $translations['header_back_label'] ?>">&#129044;</a>
+					<a href="<?= $homeSlug ?>" class="top-bar-back" aria-label="<?= $translations['header_back_label'] ?>">&#8592;</a>
 					<p class="top-bar-text"><?= $translations['header_banner_text'] ?></p>
 				</div>
 				<a href="/<?= $lang ?>/relocation.php" class="top-bar-cta"><?= $translations['header_banner_cta'] ?></a>
